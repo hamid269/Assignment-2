6c29140810831a81283f72a85801c05e3514e50e
DyinToLive <88452454+DyinToLive@users.noreply.github.com>
2022-11-18 12:29:12 -0700

    pull from repo

--- starting-files/js/assign2.js
+++ starting-files/js/assign2.js
@@ -1,13 +1,31 @@
 
+document.addEventListener("DOMContentLoaded", function () {
 
 
-/* url of song api --- https versions hopefully a little later this semester */	
-const api = 'http://www.randyconnolly.com/funwebdev/3rd/api/music/songs-nested.php';
 
- 
+    /* url of song api --- https versions hopefully a little later this semester */
+    const api = 'http://www.randyconnolly.com/funwebdev/3rd/api/music/songs-nested.php';
 
-/* note: you may get a CORS error if you try fetching this locally (i.e., directly from a
-   local file). To work correctly, this needs to be tested on a local web server.  
-   Some possibilities: if using Visual Code, use Live Server extension; if Brackets,
-   use built-in Live Preview.
-*/
+
+
+    /* note: you may get a CORS error if you try fetching this locally (i.e., directly from a
+       local file). To work correctly, this needs to be tested on a local web server.  
+       Some possibilities: if using Visual Code, use Live Server extension; if Brackets,
+       use built-in Live Preview.
+    */
+    const genres = JSON.parse(content2);
+    const artists = JSON.parse(content);
+    const songs = JSON.parse(content3);
+
+    console.log(genres);
+
+    console.log(artists);
+
+    console.log(songs);
+    const table = document.querySelector("#songTable");
+    for (let s of songs) {
+    }
+
+});
+
+    
