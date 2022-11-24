d9c4a76e721550b26986845d0bb2088a5f55d242
DyinToLive <88452454+DyinToLive@users.noreply.github.com>
2022-11-24 12:54:13 -0700

    Implemented fetch and error handling. Created functions for artist list and genre list. Working on the search filters now.

--- starting-files/js/assign2.js
+++ starting-files/js/assign2.js
@@ -1,52 +1,106 @@
 
 document.addEventListener("DOMContentLoaded", function () {
-
-
-
     /* url of song api --- https versions hopefully a little later this semester */
     const api = 'http://www.randyconnolly.com/funwebdev/3rd/api/music/songs-nested.php';
-
-
-
     /* note: you may get a CORS error if you try fetching this locally (i.e., directly from a
        local file). To work correctly, this needs to be tested on a local web server.  
        Some possibilities: if using Visual Code, use Live Server extension; if Brackets,
        use built-in Live Preview.
     */
-    const artists = JSON.parse(content);
-    const genres = JSON.parse(content2);
-    const songs = JSON.parse(content3);
-    //initially sort the songs by title as per instructions.
-    const sortByTitle = songs.sort((a, b) => a.title < b.title ? -1 : 1);
-    //display the songs when page is opened
-    displaySongs(sortByTitle);
-  
-     // "const ______RadioButton" SORTS FOR TITLE, ARTIST, YEAR, GENRE, POPULARITY BEGIN HERE.
-    //each of these event listeners sort the songs by the appropriate field.
-    const titleRadioButton = document.querySelector("#titleRadioButton").addEventListener("click", function () {
+
+    const jsonData = localStorage.getItem("songs");
+    if (!jsonData) {
+        fetch(api)
+            .then(resp => {
+                if (resp.ok) {
+                    return resp.json();
+                }
+                else {
+                    return alert("Could not fetch API");
+                }
+            }).then(data => {
+                const jsonData = JSON.stringify(data);
+                localStorage.setItem("songs", jsonData);
+            });
+    }
+    else {
+        const artists = JSON.parse(content);
+        const genres = JSON.parse(content2);
+        const songs = JSON.parse(jsonData);
+        //initially sort the songs by title as per instructions.
         const sortByTitle = songs.sort((a, b) => a.title < b.title ? -1 : 1);
+        //display the songs when page is opened
         displaySongs(sortByTitle);
-    });
+        //invoke genre list
+        genreSelectList(genres);
+        //invoke select list
+        artistSelectList(artists);
+        // "const ______RadioButton" SORTS FOR TITLE, ARTIST, YEAR, GENRE, POPULARITY BEGIN HERE.
+        //each of these event listeners sort the songs by the appropriate field.
+        const titleRadioButton = document.querySelector("#titleRadioButton").addEventListener("click", function () {
+            const sortByTitle = songs.sort((a, b) => a.title < b.title ? -1 : 1);
+            displaySongs(sortByTitle);
+        });
 
-    const artistRadioButton = document.querySelector("#artistRadioButton").addEventListener("click", function () {
-        const sortByArtist = songs.sort((a, b) => a.artist.name < b.artist.name ? -1 : 1);
-        displaySongs(sortByArtist);
-    });
-    const yearRadioButton = document.querySelector("#yearRadioButton").addEventListener("click", function () {
-        const sortByYear = songs.sort((a, b) => a.year < b.year ? -1 : 1);
-        displaySongs(sortByYear);
-    });
+        const artistRadioButton = document.querySelector("#artistRadioButton").addEventListener("click", function () {
+            const sortByArtist = songs.sort((a, b) => a.artist.name < b.artist.name ? -1 : 1);
+            displaySongs(sortByArtist);
+        });
+        //year sorted from newest to oldest
+        const yearRadioButton = document.querySelector("#yearRadioButton").addEventListener("click", function () {
+            const sortByYear = songs.sort((a, b) => a.year > b.year ? -1 : 1);
+            displaySongs(sortByYear);
+        });
 
-    const genreRadioButton = document.querySelector("#genreRadioButton").addEventListener("click", function () {
-        const sortByGenre = songs.sort((a, b) => a.genre.name < b.genre.name ? -1 : 1);
-        displaySongs(sortByGenre);
-    });
+        const genreRadioButton = document.querySelector("#genreRadioButton").addEventListener("click", function () {
+            const sortByGenre = songs.sort((a, b) => a.genre.name < b.genre.name ? -1 : 1);
+            displaySongs(sortByGenre);
+        });
+        //popularity sorted from highest to lowest
+        const popRadioButton = document.querySelector("#popRadioButton").addEventListener("click", function () {
+            const sortByPop = songs.sort((a, b) => a.details.popularity > b.details.popularity ? -1 : 1);
+            displaySongs(sortByPop);
+        });
+        //end of click events for the sort buttons on the main table.
+      
+    }
+    /*
+     * function name: artistSelectList
+     * parameters: artists json data
+     * DROPDOWN SELECT LIST FOR ARTISTS
+     * this function recieves artist json data, builds option elements for each artist,
+     * and appends the option to the select list with the id artistSelect in the index.html page
+     * */
 
-    const popRadioButton = document.querySelector("#popRadioButton").addEventListener("click", function () {
-        const sortByPop = songs.sort((a, b) => a.details.popularity > b.details.popularity ? -1 : 1);
-        displaySongs(sortByPop);
-    });
-    //end of click events for the sort buttons on the main table.
+    function artistSelectList(artists) {
+        const artistSelect = document.querySelector("#artistSelect");
+        for (let a of artists) {
+            const artistOption = document.createElement("option");
+            artistOption.value = a.id;
+            artistOption.textContent = a.name;
+            artistSelect.appendChild(artistOption);
+        }
+    }
+    /*
+     * function name: genreSelectList
+     * parameters: genre json data
+     * DROPDOWN SELECT LIST FOR GENRES
+     * this function recieves genre json data, builds option elements for each genre,
+     * and appends the option to the select list with the id genreSelect in the index.html page
+     * */
+    function genreSelectList(genres) {
+        const genreSelect = document.querySelector("#genreSelect");
+        for (let g of genres) {
+            const genreOption = document.createElement("option");
+            genreOption.value = g.id;
+            genreOption.textContent = g.name;
+            genreSelect.appendChild(genreOption);
+        }
+    }
+    
+ 
+  
+  
 
   /*
     //attempt to search songs with title, artist, and genre
@@ -70,8 +124,11 @@ document.addEventListener("DOMContentLoaded", function () {
         displaySongs(searchList);
     });
    */
-    //function name: displaySongs. This function creates a table and its elements and displays
-    //the passed in song object in the form title, artist, year, genre, popularity. 
+
+    //function name: displaySongs. 
+    //Parameters: a song object. sorts are done on the songs data then passed in to the function.
+    //This function creates a table and its elements and displays
+    //the passed in song object, and displays it in a table in the form title, artist, year, genre, popularity. 
     function displaySongs(sortedSongs) {
         
         const tableBody = document.querySelector(".allSongsBody");
@@ -144,30 +201,7 @@ document.addEventListener("DOMContentLoaded", function () {
     */
     function populateSongView(e) {
         let clickedSongTitle = e.target.getAttribute('title');
-
-        const findSong = songs.find(function (element) {
-            return element.title === clickedSongTitle;
-        });
-
-
+        const findSong = songs.find(s => s.id == e.target.dataset.songId);
     }
-   // console.log(genres);
-    //create a dropdown list of options for genres
-    const genreSelect = document.querySelector("#genreSelect");
-    for (let g of genres) {
-        const genreOption = document.createElement("option");
-        genreOption.value = g.id;
-        genreOption.textContent = g.name;
-        genreSelect.appendChild(genreOption);
-    }
-   // console.log(artists);
-    //create a dropdown list of options for artists
-    const artistSelect = document.querySelector("#artistSelect");
-    for (let a of artists) {
-        const artistOption = document.createElement("option");
-        artistOption.value = a.id;
-        artistOption.textContent = a.name;
-        artistSelect.appendChild(artistOption);
-    }
-    
+
 });
